e547ef304582650684081cc3fbfbafc664f3a537
TheScarastic <warabhishek@gmail.com>
2019-07-16 23:07:55 +0530

    riva: Update additional graphics properties for P
    * fixes skia glitch issue
    
    Change-Id: I4f7f5319f994cdcd6525b6e0c05eae9f5fcb0cb1

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -83,6 +83,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608 \
     ro.vendor.display.cabl=2 \
     ro.sf.lcd_density=280 \
+    debug.sf.disable_backpressure=1 \
     vendor.display.enable_default_color_mode=1
 
 # Fm
